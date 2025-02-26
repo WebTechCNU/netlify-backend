d5b73849120287db271de787a13d2c056fc5648b
Vasyl Melnyk <windchange7@gmail.com>
2025-02-26 19:49:58 +0200

    skip take try 0

--- functions/listQuestions.js
+++ functions/listQuestions.js
@@ -3,12 +3,20 @@ const connectDB = require("./db");
 exports.handler = async (event) => {
   try {
     const collection = await connectDB();
-    const { topic, sortField, sortOrder = "asc" } = event.queryStringParameters;
+    const { topic, sortField, sortOrder = "asc", skip, take } = event.queryStringParameters;
 
     const query = topic ? { topic: new RegExp(topic, "i") } : {};
     const sort = sortField ? { [sortField]: sortOrder === "desc" ? -1 : 1 } : {};
 
-    const questions = await collection.find(query).sort(sort).toArray();
+    const skipValue = Number.isInteger(parseInt(skip)) ? parseInt(skip) : 0;
+    const takeValue = Number.isInteger(parseInt(take)) ? parseInt(take) : 20;
+
+    const questions = await collection
+      .find(query)
+      .sort(sort)
+      .skip(skipValue)
+      .limit(takeValue)
+      .toArray();
 
     return { 
       statusCode: 200, 
